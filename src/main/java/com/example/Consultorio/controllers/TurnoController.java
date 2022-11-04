9370627f6c534d478ce745b79368453ec1dcc374
DaiCurcio <daianacurcio@hotmail.com>
2022-11-04 08:36:50 -0300

    modificacion 4/11

--- src/main/java/com/example/Consultorio/controllers/TurnoController.java
+++ src/main/java/com/example/Consultorio/controllers/TurnoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/Turno")
 public class TurnoController {
     @Autowired
     TurnoService turnoService;
